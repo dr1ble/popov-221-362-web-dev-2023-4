7f2fcca6c8d1e47782ec711dfde77d897c6f89da
dr1ble <nik.qaz321@gmail.com>
2023-10-10 22:18:01 +0300

    default

--- index.php
+++ index.php
@@ -4,11 +4,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $message = $_POST['message'];
     $topic = $_POST['topic'];
+    $file = $_POST['file'];
     $consent = isset($_POST['consent']) ? 'Да' : 'Нет';
     $source = $_POST['source'];
+    
 
     // Перенаправление на страницу home.php для вывода ответа.
-    header("Location: home.php?fio=$fio&message=$message&source=$source");
+    header("Location: home.php?fio=$fio&message=$message&source=$source&file=$file");
     exit();
 }
 ?>
