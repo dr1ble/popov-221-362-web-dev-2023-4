42a7938ddcfc63693315d6c1d65d03fbe94170ea
dr1ble <nik.qaz321@gmail.com>
2023-10-10 22:56:29 +0300

    rework

--- home.php
+++ home.php
@@ -1,16 +1,20 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Ответ на обращение</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+<?php
+include 'header.html';
+?>
+
 <body>
     <?php
-    $fio = isset($_GET['fio']) ? $_GET['fio'] : '';
-    $message = isset($_GET['message']) ? $_GET['message'] : '';
-    $source = isset($_GET['source']) ? $_GET['source'] : '';
-    $file = isset($_GET['file']) ? $_GET['file'] : '';
+    if (isset($_POST['fio'])) {
+        $fio = $_POST['fio'];
+        $file = $_POST['file'];
+        $message = $_POST['message'];
+        $topic = $_POST['topic'];
+        if (isset($_POST['source'])) {
+            $source = $_POST['source'];
+        } else {
+            $source = '';
+        }
+    }
 
     echo "<h1>Ответ на ваше обращение:</h1>";
     echo "<p><h2>ФИО:</h2> <span>$fio</span></p>";
@@ -18,9 +22,8 @@
     echo "<p><h2>Источник:</h2> <span>$source</span></p></h2>";
     echo "<p><h2>Вы прикрепили файл:</h2><span>$file</span></p></h2>";
 
-    echo '<form action="header.html">';
-    echo '<input type="submit" value="Заполнить снова">';
-    echo '</form>';
+    echo '<br><a class="button" href="index.php?N=' . $_POST['fio'] . '&E=' . $_POST['email'] . '&R='.$source. '">Заполнить снова</a></p></br>';
     ?>
 </body>
-</html>
+
+</html> 
